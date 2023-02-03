4c7ed4913e8204aae6c9b4dc18a25f10ed8adb5d
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-02-03 17:21:00 +0530

    about section done

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8 ">
+                <h1 className="text-4xl font-bold inline border-b-4 border-gray-500">About</h1>
+            </div>
+            <p className="text-xl mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam nulla, porro,  saepe optio ex voluptatibus veniam, laudantium velit possimus repellat inventore quaerat    cumque magnam obcaecati itaque magni reprehenderit molestiae!
+            </p>
+            <br />
+            <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, possimus rem eos odit doloribus id ipsa facilis non illo. Tenetur?
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About;
