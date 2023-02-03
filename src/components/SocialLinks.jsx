5fc2eb315027d5d15501a6b1645258b2d1431e2c
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-02-03 13:36:16 +0530

    social links done

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,6 +1,6 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
 
 
 const SocialLinks = () => {
@@ -32,21 +32,21 @@ const SocialLinks = () => {
                     Mail <HiOutlineMail size={30} />
                 </>
             ),
-            href: "mailto:techdefaulters@gmail.com"
+            href: "mailto:techdefaulters@gmail.com",
+            style: "rounded-br-md"
         }
     ]
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
         <ul>
             {links.map((link) => (
-                <li key={link.id} className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md   duration-300 bg-gray-500">
-                    <a href="" className="flex justify-between items-center w-full text-white">
-                        
+                <li key={link.id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md   duration-300 bg-gray-500 " + link.style}>
+                    <a href={link.href} className="flex justify-between items-center w-full text-white" target="_blank" rel="noreferrer">
+                        {link.child}
                     </a>
                 </li>
-            ))}
-            
+            ))}            
         </ul>
     </div>
   )
