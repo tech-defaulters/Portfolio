13ae579f5e022dedba9b027d2ec2a0ea8fbfe044
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-02-09 23:11:08 +0530

    footer section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@
 
 function Footer() {
   return (
-    <div className="bg-gray-800 text-white">
-        <h1>Footer</h1>
+    <div className="bg-gray-800 text-white p-20">
+        <div className="flex flex-col justify-center items-center">
+			<div className="flex flex-col md:flex-row">
+				<p>Copyright &copy; 2023</p>
+				<p>&nbsp; Tech Defaulters.</p>
+			</div>
+			<div>
+				<p>All rights reserved.</p>
+			</div>
+        </div>
     </div>
   )
 }
