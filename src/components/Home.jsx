0a6502ee5015552fd280052c9fd3e147735feed5
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-02-09 23:21:02 +0530

    home section updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ return (
 		<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
 			<div className="flex flex-col justify-center h-full">
 				<h2 className="text-4xl sm:text-7xl font-bold text-white">
-					Lorem ipsum dolor sit amet.
+				Make your business shine on the web.
 				</h2>
 				<p className="text-gray-500 py-4 max-w-md">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias incidunt eligendi illum velit quod minus nobis suscipit, perferendis sint!
+					We work in a creative way to make your website a powerful tool for your business. We offer perfection in every web design we create.
 				</p>
 				<div>
 					<Link to="Portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
