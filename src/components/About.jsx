f5890dac87dc83dd53b74d6e28e13699bbeab69d
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-02-16 12:43:29 +0530

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
             <div className="pb-8 ">
                 <h1 className="text-4xl font-bold inline border-b-4 border-gray-500">About</h1>
             </div>
-            <p className="text-xl mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam nulla, porro,  saepe optio ex voluptatibus veniam, laudantium velit possimus repellat inventore quaerat    cumque magnam obcaecati itaque magni reprehenderit molestiae!
+            <p className="text-xl mt-20">We are a website development company based in India. We do what we do because we love technology. Our services makes it easy for everyone to be online with an attractive, efficient and utilitarian web presence. No innovation limits, no coding - just total opportunity to tell what needs to be done and tackle your entire business on the web.
             </p>
             <br />
-            <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, possimus rem eos odit doloribus id ipsa facilis non illo. Tenetur?
+            <p className="text-xl">Our team is made up of developers, all of whom are dedicated to making sure that your projects run smoothly.
             </p>
         </div>
     </div>
